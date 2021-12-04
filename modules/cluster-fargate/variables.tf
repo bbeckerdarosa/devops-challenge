96220893acd3fd6f6366fb3feb9daf51b493291f
Bárbara Becker <bbeckerdarosa@gmail.com>
2021-12-04 17:03:24 +0100

    feat: initial structure terraform modules

---
+++ modules/cluster-fargate/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "Region to deploy cluster in AWS"
+  type        = string
+}
+
+variable "aws_access_key" {
+  description = "This is the AWS access key"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "This is the AWS secret key"
+  type        = string
+}
+
