d7cbbcc2b0563893feaf844084cd4fa157f40829
Bárbara Becker <bbeckerdarosa@gmail.com>
2021-12-06 00:57:23 +0100

    fix: descriptions of variables input

--- modules/cluster-fargate/variables.tf
+++ modules/cluster-fargate/variables.tf
@@ -1,15 +1,15 @@
 variable "region" {
-  description = "Region to deploy cluster in AWS"
+  description = "Region to deploy the Cluster ECS Fargate in your AWS account"
   type        = string
 }
 
 variable "aws_access_key" {
-  description = "This is the AWS access key"
+  description = "Please, insert your AWS access key from your account"
   type        = string
 }
 
 variable "aws_secret_key" {
-  description = "This is the AWS secret key"
+  description = "Please, insert your AWS secret key from your account"
   type        = string
 }
 
@@ -25,14 +25,6 @@ variable "env" {
   type        = string
 }
 
-variable "cluster_name" {
-  type        = string
-}
-
-variable "capacity_providers" {
-  type        = string
-}
-
 variable "container_name" {
   type        = string
 }
@@ -41,14 +33,20 @@ variable "container_port" {
   type        = string
 }
 
-variable "container_port" {
+variable "vpc_id" {
+  description = "Please, insert VPC id from your AWS account"
   type        = string
 }
 
 variable "subnets"       { 
-  type        = list(string) 
+  description = "Please, insert the list of subnets from your VCP id. Ex: [subnet_id1, subnet_id2, subnet_id3]"
+  type        = list(string)
 }
 
-variable "vpc_id" {
+variable "launch_type" {
+  type        = string
+}
+
+variable "network_mode" {
   type        = string
 }
