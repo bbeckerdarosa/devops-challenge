d6dbb6f5376f1c34db5ddb1ee7d81cdcfc7fedc3
Bárbara Becker <bbeckerdarosa@gmail.com>
2021-12-05 21:26:59 +0100

    feat: add load balancer config to the application

---
+++ modules/cluster-fargate/load-balancer.tf
@@ -0,0 +1,24 @@
+resource "aws_lb" "application_lb" {
+  name               = var.cluster_name-var.env
+  subnets            = var.subnets
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.application_alb_sg.id]
+}
+
+resource "aws_lb_target_group" "application_tg" {
+  name     = var.cluster_name-var.env
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = var.vpc_id
+}
+
+resource "aws_lb_listener" "application_listener" {
+  load_balancer_arn = aws_lb.application_lb.id
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    target_group_arn = aws_lb_target_group.application_tg.id
+    type             = "forward"
+  }
+}
