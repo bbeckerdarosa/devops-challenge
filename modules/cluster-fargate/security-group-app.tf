7f33d373efd7d50ef345a486fcf72cefb5811bca
Bárbara Becker <bbeckerdarosa@gmail.com>
2021-12-05 21:36:12 +0100

    feat: add security group to the application ecs service

---
+++ modules/cluster-fargate/security-group-app.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "application_sg" {
+  name        = var.cluster_name-var.env-alb
+  vpc_id      = var.vpc_id
+
+  ingress {
+    protocol        = "tcp"
+    from_port       = var.container_port
+    to_port         = var.container_port
+    security_groups = [aws_security_group.application_alb_sg.id]
+  }
+
+  egress {
+    protocol    = "-1"
+    from_port   = 0
+    to_port     = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
