1b8ba2197fc72b1797ac62e078828e628deeec18
Bárbara Becker <bbeckerdarosa@gmail.com>
2021-12-06 01:19:05 +0100

    refactor: remove terraform backend s3

--- stage/cluster-fargate/main.tf
+++ stage/cluster-fargate/main.tf
@@ -1,20 +1,11 @@
-terraform {
-  backend "s3" {
-    bucket = "cluster-ecs-fargate"
-    key    = "stage/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 module "cluster-fargate" {
   source                = "../../modules/cluster-fargate/"
 
   env                   = "stage"
-  bucket                = "cluster-ecs-fargate"
   region                = "us-east-1"
   cluster_name          = "fargate"
   capacity_providers    = "FARGATE"
