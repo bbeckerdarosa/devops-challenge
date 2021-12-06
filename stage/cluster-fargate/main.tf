752468995061328ccbb16a693ea474867df21fb3
Bárbara Becker <bbeckerdarosa@gmail.com>
2021-12-06 14:45:21 +0100

    fix: add variables to enter a value when to run  terraform plan command

--- stage/cluster-fargate/main.tf
+++ stage/cluster-fargate/main.tf
@@ -1,16 +1,39 @@
+variable "aws_access_key" {
+  description = "Please, insert your AWS access key from your account"
+  type        = string
+}
+
+variable "aws_secret_key" {
+  description = "Please, insert your AWS secret key from your account"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "Please, insert VPC id from your AWS account"
+  type        = string
+}
+
+variable "subnets"       { 
+  description = "Please, insert the list of subnets from your VCP id. Ex: [subnet_id1, subnet_id2, subnet_id3]"
+  type        = list(string)
+}
+
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 module "cluster-fargate" {
   source                = "../../modules/cluster-fargate/"
-
+  
   env                   = "stage"
   region                = "us-east-1"
   cluster_name          = "fargate"
-  capacity_providers    = "FARGATE"
   launch_type           = "FARGATE"
   container_name        = "fargate-application-version"
   container_port        = 3000
   network_mode          = "awsvpc"
+  vpc_id                = var.vpc_id
+  subnets               = var.subnets
 }
