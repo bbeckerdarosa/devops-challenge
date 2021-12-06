d2b06679482da08195bbf422dd07907bd752d2c7
Bárbara Becker <bbeckerdarosa@gmail.com>
2021-12-06 14:43:20 +0100

    refactor: remove some variables and sent to the main stage file

--- modules/cluster-fargate/variables.tf
+++ modules/cluster-fargate/variables.tf
@@ -3,21 +3,7 @@ variable "region" {
   type        = string
 }
 
-variable "aws_access_key" {
-  description = "Please, insert your AWS access key from your account"
-  type        = string
-}
-
-variable "aws_secret_key" {
-  description = "Please, insert your AWS secret key from your account"
-  type        = string
-}
-
 variable "cluster_name" {
-  type        = string
-}
-
-variable "capacity_providers" {
   type        = string
 }
 
@@ -33,6 +19,14 @@ variable "container_port" {
   type        = string
 }
 
+variable "launch_type" {
+  type        = string
+}
+
+variable "network_mode" {
+  type        = string
+}
+
 variable "vpc_id" {
   description = "Please, insert VPC id from your AWS account"
   type        = string
@@ -41,12 +35,4 @@ variable "vpc_id" {
 variable "subnets"       { 
   description = "Please, insert the list of subnets from your VCP id. Ex: [subnet_id1, subnet_id2, subnet_id3]"
   type        = list(string)
-}
-
-variable "launch_type" {
-  type        = string
-}
-
-variable "network_mode" {
-  type        = string
 }
