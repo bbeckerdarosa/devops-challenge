f285a5d71bc76dd6d8c546376078bfd821c69676
Bárbara Becker <bbeckerdarosa@gmail.com>
2021-12-10 16:28:33 +0100

    fix: create dedicate subnet and fixed into resources

--- modules/cluster-fargate/load-balancer.tf
+++ modules/cluster-fargate/load-balancer.tf
@@ -1,25 +1,25 @@
 resource "aws_lb" "application_lb" {
   name               = "${var.cluster_name}-${var.env}"
-  subnets            = var.subnets
+  subnets            = aws_subnet.public.*.id
   load_balancer_type = "application"
   security_groups    = [aws_security_group.application_alb_sg.id]
 }
 
 resource "aws_lb_target_group" "application_tg" {
   name        = "${var.cluster_name}-${var.env}"
-  port        = 80
+  port        = 3000
   protocol    = "HTTP"
   target_type = "ip"
-  vpc_id      = var.vpc_id
+  vpc_id      = aws_vpc.new.id
 }
 
 resource "aws_lb_listener" "application_listener" {
-  load_balancer_arn = aws_lb.application_lb.id
+  load_balancer_arn = aws_lb.application_lb.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = aws_lb_target_group.application_tg.id
+    target_group_arn = aws_lb_target_group.application_tg.arn
     type             = "forward"
   }
 }
